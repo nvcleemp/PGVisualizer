74b604b3f1a483e3982f5add617cf32d3a616eef
nvcleemp <nico.vancleemput@gmail.com>
2008-02-13 09:33:14 +0000

    Added two embedders that change the angle of the fundamental domain

---
+++ src/azul/toroidalembedder/DomainAngleEmbedder.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package azul.toroidalembedder;
+
+import azul.toroidalembedder.energy.EnergyCalculator;
+import azul.toroidalembedder.graph.Edge;
+import azul.toroidalembedder.graph.FundamentalDomain;
+import azul.toroidalembedder.graph.Graph;
+import azul.toroidalembedder.graph.Vertex;
+
+/**
+ *
+ * @author nvcleemp
+ */
+public class DomainAngleEmbedder implements Embedder {
+    
+    private Graph graph;
+    private int intervals;
+    private int zoom;
+    private double initialSearchAngle;
+    private double currentSearchAngle;
+    private boolean lock = false;
+    private EnergyCalculator energyCalculator;
+
+    public DomainAngleEmbedder(Graph graph, int intervals, int zoom, double initialSearchAngle, EnergyCalculator energyCalculator) {
+        this.graph = graph;
+        this.intervals = intervals;
+        this.zoom = zoom;
+        this.initialSearchAngle = initialSearchAngle;
+        this.energyCalculator = energyCalculator;
+    }
+
+    public void initialize() {
+        currentSearchAngle = initialSearchAngle;
+    }
+
+    public void embed() {
+        if(lock)
+            return;
+        lock = true;
+        double minAngle = graph.getFundamentalDomain().getAngle();
+        double minCost = energyCalculator.calculateEnergy(graph, graph.getFundamentalDomain());
+        
+        double startAngle = graph.getFundamentalDomain().getAngle() - currentSearchAngle/2;
+        double endAngle = graph.getFundamentalDomain().getAngle() + currentSearchAngle/2;
+        double increment = currentSearchAngle/intervals;
+        if(startAngle<=0) startAngle = increment;
+        if(endAngle>=Math.PI) endAngle = Math.PI - increment;
+        
+        FundamentalDomain domain = new FundamentalDomain(startAngle, graph.getFundamentalDomain().getHorizontalSide(), graph.getFundamentalDomain().getVerticalSide());
+        while(domain.getAngle()<endAngle){
+            double cost = energyCalculator.calculateEnergy(graph, domain);
+            if(cost < minCost){
+                minAngle = domain.getAngle();
+                minCost = cost;
+            }
+            domain.addToAngle(increment);
+        }
+        if(graph.getFundamentalDomain().getAngle()!=minAngle)
+            graph.setFundamentalDomain(new FundamentalDomain(minAngle, graph.getFundamentalDomain().getHorizontalSide(), graph.getFundamentalDomain().getVerticalSide()));
+        currentSearchAngle /= zoom;
+        lock = false;
+    }
+}
---
+++ src/azul/toroidalembedder/FastDomainAngleEmbedder.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package azul.toroidalembedder;
+
+import azul.toroidalembedder.energy.EnergyCalculator;
+import azul.toroidalembedder.graph.Edge;
+import azul.toroidalembedder.graph.FundamentalDomain;
+import azul.toroidalembedder.graph.Graph;
+import azul.toroidalembedder.graph.Vertex;
+
+/**
+ *
+ * @author nvcleemp
+ */
+public class FastDomainAngleEmbedder implements Embedder {
+    
+    private Graph graph;
+    private double epsilon;
+    private double k;
+    private EnergyCalculator energyCalculator;
+
+    public FastDomainAngleEmbedder(Graph graph, double epsilon, double k, EnergyCalculator energyCalculator) {
+        this.graph = graph;
+        this.epsilon = epsilon;
+        this.k = k;
+        this.energyCalculator = energyCalculator;
+    }
+
+    public void initialize() {
+        //
+    }
+
+    public void embed() {
+        double energy = energyCalculator.calculateEnergy(graph, graph.getFundamentalDomain());
+        double angleE = graph.getFundamentalDomain().getAngle() - epsilon > 0 ? graph.getFundamentalDomain().getAngle() - epsilon : epsilon;
+        double energy2 = energyCalculator.calculateEnergy(graph, new FundamentalDomain(angleE, graph.getFundamentalDomain().getHorizontalSide(), graph.getFundamentalDomain().getVerticalSide()));
+        double angle = graph.getFundamentalDomain().getAngle() + (energy2 - energy)*epsilon*k;
+        if(angle <=0)
+            angle = epsilon;
+        else if(angle >=Math.PI)
+            angle = Math.PI - epsilon;
+        graph.setFundamentalDomain(new FundamentalDomain(angle, graph.getFundamentalDomain().getHorizontalSide(), graph.getFundamentalDomain().getVerticalSide()));
+    }
+}
