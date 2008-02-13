58c8e50794ab0d99cd10121912b1273a245843c0
nvcleemp <nico.vancleemput@gmail.com>
2008-02-13 09:24:12 +0000

    added support for energy calculation

---
+++ src/azul/toroidalembedder/energy/EnergyCalculator.java
@@ -0,0 +1,17 @@
+/*
+ * EnergyCalculator.java
+ */
+
+package azul.toroidalembedder.energy;
+
+import azul.toroidalembedder.graph.FundamentalDomain;
+import azul.toroidalembedder.graph.Graph;
+
+/**
+ * Calculates an energy for a given graph embedded with a given fundamental domain
+ */
+public interface EnergyCalculator {
+    
+    public double calculateEnergy(Graph graph, FundamentalDomain domain);
+
+}
---
+++ src/azul/toroidalembedder/energy/MeanEdgeLengthEnergyCalculator.java
@@ -0,0 +1,39 @@
+/*
+ * MeanEdgeLengthEnergyCalculator.java
+ */
+
+package azul.toroidalembedder.energy;
+
+import azul.toroidalembedder.graph.Edge;
+import azul.toroidalembedder.graph.FundamentalDomain;
+import azul.toroidalembedder.graph.Graph;
+import azul.toroidalembedder.graph.Vertex;
+
+/**
+ * Calculates an energy based on the difference of length between the edges.
+ */
+public class MeanEdgeLengthEnergyCalculator implements EnergyCalculator{
+
+    public double calculateEnergy(Graph graph, FundamentalDomain domain) {
+        double mean = 0;
+        int count = 0;
+        for (Vertex vertex : graph.getVertices()) {
+            for (Edge edge : vertex.getEdges()) {
+                mean += graph.getEdgeLength(edge, domain);
+                count++;
+            }
+        }
+        mean /= count;
+        
+        double energy=0;
+        for (Vertex vertex : graph.getVertices()) {
+            for (Edge edge : vertex.getEdges()) {
+                double d = graph.getEdgeLength(edge, domain);
+                energy += (mean - d) * (mean - d);
+            }
+        }
+        
+        return energy;
+    }
+
+}
