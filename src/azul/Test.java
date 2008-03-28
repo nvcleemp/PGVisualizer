4103e6a62fb4f7f84a5b35623481ba93a102a032
nvcleemp <nico.vancleemput@gmail.com>
2008-03-28 09:14:36 +0000

    added some more uses to demo

--- src/azul/Test.java
+++ src/azul/Test.java
@@ -5,6 +5,7 @@
 
 package azul;
 
+import azul.delaney.FundamentalPatch;
 import azul.delaney.DelaneySymbol;
 import azul.delaney.Utility;
 import azul.io.FileFormatException;
@@ -63,7 +64,19 @@ public class Test {
         button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 try {
+                    //new EmbedderWindow(IOManager.readTorGraph("10|2 2|0 0;0 0;0 0;0 0;0 0;0 0;0 0;0 0;0 0;0 0|0 8 0 0;8 1 0 0;1 2 0 0;2 3 0 0;3 9 0 0;9 4 0 0;4 5 0 0;5 6 0 0;6 7 0 0;7 0 0 0;0 5 0 -1;1 4 0 -1;2 7 1 0;3 6 1 0;8 9 0 0")).setVisible(true);
                     new EmbedderWindow(IOManager.readTorGraph("10|2 2|0 0;0 0;0 0;0 0;0 0;0 0;0 0;0 0;0 0;0 0|0 8 0 0;8 1 0 0;1 2 0 0;2 3 0 0;3 9 0 0;9 4 0 0;4 5 0 0;5 6 0 0;6 7 0 0;7 0 0 0;0 5 0 -1;1 4 0 -1;2 7 1 0;3 6 1 0;8 9 0 0")).setVisible(true);
+                } catch (FileFormatException ex) {
+                    Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+        frame.add(button);
+        button = new JButton("Embedder test: azulene 2");
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    new EmbedderWindow(IOManager.readTorGraph("10|2 2|0 0;0 0;0 0;0 0;0 0;0 0;0 0;0 0;0 0;0 0|0 8 0 0;8 1 0 0;1 2 0 0;2 3 0 0;3 9 0 0;9 4 0 0;4 5 0 0;5 6 0 0;6 7 0 0;7 0 0 0;0 4 0 1;2 6 -1 0;3 5 -1 0;1 7 -1 0;8 9 0 0")).setVisible(true);
                 } catch (FileFormatException ex) {
                     Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
                 }
@@ -81,7 +94,36 @@ public class Test {
                                 new EmbedderWindow(Utility.delaneyToTorGraph(symbol)).setVisible(true);
                             } catch (Exception exc){
                                 symbol.printSymbol();
+                                exc.printStackTrace();
+                            } catch (Error er){
+                                symbol.printSymbol();
+                                er.printStackTrace();
                             }
+            }
+        });
+        frame.add(button);
+        button = new JButton("Create patch");
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser chooser = new JFileChooser();
+                if(chooser.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION)
+                    if(chooser.getSelectedFile().exists()){
+                        int count = 0;
+                        int succes = 0;
+                        for(DelaneySymbol symbol: IOManager.readDS(chooser.getSelectedFile()))
+                            try {
+                                count++;
+                                FundamentalPatch tree = FundamentalPatch.createBFSTree(symbol);
+                                tree.print();
+                                tree.closeOrbits();
+                                tree.print();
+                                succes++;
+                            } catch (Exception exc){
+                                
+                            } catch (Error er){
+
+                            }
+                    }
             }
         });
         frame.add(button);
