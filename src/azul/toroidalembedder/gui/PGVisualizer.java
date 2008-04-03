4dfb8fb3ceafee08646dbe386d5ccddb8bbb05f5
nvcleemp <nico.vancleemput@gmail.com>
2008-04-03 13:24:20 +0000

    added embedder controls to PGVisualizer

--- src/azul/toroidalembedder/gui/PGVisualizer.java
+++ src/azul/toroidalembedder/gui/PGVisualizer.java
@@ -5,20 +5,12 @@
 
 package azul.toroidalembedder.gui;
 
-import azul.toroidalembedder.embedder.Embedder;
-import azul.toroidalembedder.embedder.SpringEmbedder;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import javax.swing.ButtonModel;
 import javax.swing.JButton;
 import javax.swing.JPanel;
-import javax.swing.Timer;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 
 /**
  *
@@ -28,49 +20,17 @@ public class PGVisualizer extends JPanel{
     
     private GraphModel model;
     private TorusView view = new TorusView();
-    private Embedder embedder;
-    private ActionListener embedAction = new ActionListener() {
-        public void actionPerformed(ActionEvent e) {
-            if(embedder!=null)
-                embedder.embed();
-        }
-    };
-    private Timer timer = new Timer(100, embedAction);
-    private ChangeListener changeListener = new ChangeListener() {
-        public void stateChanged(ChangeEvent e) {
-            JButton button = (JButton)(e.getSource());
-            ButtonModel model = button.getModel();
-            if (model != null) {
-                if (model.isPressed()) {
-                    if (!timer.isRunning()) {
-                        if(embedder!=null)
-                            embedder.initialize();
-                        timer.start();
-                    }
-                } else if (timer.isRunning()) {
-                    timer.stop();
-                }
-            }
-        }
-    };
+
     public PGVisualizer(File file) {
         model = new GraphModel(file);
         setLayout(new BorderLayout());
         add(new ListSelectionNavigator(model.getSelectionModel(), model), BorderLayout.NORTH);
         view = new TorusView(model);
-        embedder = new SpringEmbedder(model.getSelectedGraph());
-        model.addListSelectionListener(new ListSelectionListener() {
-            public void valueChanged(ListSelectionEvent e) {
-                embedder = new SpringEmbedder(model.getSelectedGraph());
-            }
-        });
         add(view, BorderLayout.CENTER);
         JPanel controls = new JPanel();
         controls.add(new ViewController(view));
         controls.add(new GraphOperations(model));
-        JButton run = new JButton("Run");
-        run.addChangeListener(changeListener);
-        controls.add(run);
+        controls.add(new EmbedderControl(model));
         JButton output = new JButton("Output");
         output.addActionListener(new ActionListener() {
 
