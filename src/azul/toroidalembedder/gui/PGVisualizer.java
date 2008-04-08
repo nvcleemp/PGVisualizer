161ff55397eec01be3e299be55d0f1a4e7a2bc89
nvcleemp <nico.vancleemput@gmail.com>
2008-04-08 08:12:20 +0000

    revert previous commit

--- src/azul/toroidalembedder/gui/PGVisualizer.java
+++ src/azul/toroidalembedder/gui/PGVisualizer.java
@@ -11,7 +11,6 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.File;
 import javax.swing.BoxLayout;
-import javax.swing.JButton;
 import javax.swing.JPanel;
 
 /**
@@ -22,8 +21,6 @@ public class PGVisualizer extends JPanel{
     
     private GraphModel model;
     private TorusView view = new TorusView();
-    private JPanel controls;
-    private JPanel embedderBatch;
 
     public PGVisualizer(File file) {
         model = new GraphModel(file);
@@ -44,24 +41,16 @@ public class PGVisualizer extends JPanel{
                         view.getGraphSelectionModel().addVertex(vv.vertex);
                     }
                 }
-            }            
+            }
         });
         add(view, BorderLayout.CENTER);
-        controls = new JPanel();
+        JPanel controls = new JPanel();
         controls.setLayout(new BoxLayout(controls, BoxLayout.Y_AXIS));
         controls.add(new ViewController(view));
         controls.add(new GraphOperations(model));
         controls.add(new EmbedderControl(model));
         controls.add(new ExportControl(view, model));
         add(controls, BorderLayout.EAST);
-        embedderBatch = new EmbedderBatch(model);
-        //embedderBatch.setVisible(false);
-        add(embedderBatch, BorderLayout.WEST);       
     }
     
-    public void toggleManualAuto(){
-        boolean visible = embedderBatch.isVisible();
-        embedderBatch.setVisible(!visible);
-        controls.setVisible(visible);
-    }
 }
