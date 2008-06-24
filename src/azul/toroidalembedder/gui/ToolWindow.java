bd65e49f498343b237bdb134144c9ed9c5d4a42c
nvcleemp <nico.vancleemput@gmail.com>
2008-06-24 13:29:40 +0000

    added constructor for ToolWindow

--- src/azul/toroidalembedder/gui/ToolWindow.java
+++ src/azul/toroidalembedder/gui/ToolWindow.java
@@ -5,6 +5,7 @@
 
 package azul.toroidalembedder.gui;
 
+import java.awt.Frame;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import javax.swing.JFrame;
@@ -17,11 +18,17 @@ import javax.swing.JPanel;
 public class ToolWindow extends JFrame {
 
     public ToolWindow(JPanel toolPanel, JFrame parentWindow) {
+        this(toolPanel, parentWindow, false);
+    }
+    
+    public ToolWindow(JPanel toolPanel, JFrame parentWindow, boolean maximize) {
         super(toolPanel.getName());
         parentWindow.addWindowListener(new ParentWindowListener());
         add(toolPanel);
         setAlwaysOnTop(true);
         pack();
+        if(maximize)
+            setExtendedState(Frame.MAXIMIZED_BOTH);
     }
 
     private class ParentWindowListener extends WindowAdapter{
