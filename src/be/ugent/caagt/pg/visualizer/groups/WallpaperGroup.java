2675b3244f292b1ff92bf831397fef26da8d089c
nvcleemp <nico.vancleemput@gmail.com>
2009-02-17 15:14:54 +0000

    Added enum for wallpaper groups

---
+++ src/be/ugent/caagt/pg/visualizer/groups/WallpaperGroup.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package be.ugent.caagt.pg.visualizer.groups;
+
+/**
+ *
+ * @author nvcleemp
+ */
+public enum WallpaperGroup {
+    P6M, P6, P4M, P4G, P4, P31M, P3M1, P3, CMM, PMM, PMG, PGG, P2, CM, PM, PG, P1, UNKNOWN;
+}
